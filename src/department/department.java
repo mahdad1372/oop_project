e8bbacd92b03d01763930f78dc5d2460d59f824c
unknown <mnaderiniya@gmail.com>
2023-07-02 06:15:47 -0700

    adding department section

---
+++ src/department/department.java
@@ -0,0 +1,11 @@
+package department;
+
+public class department {
+    private int department_id;
+    private String depatment_name;
+
+    public void department_description(){
+        System.out.println("Welcome to the section of the department");
+        System.out.println("Please choose the which type of the operation you want to do");
+    }
+}
